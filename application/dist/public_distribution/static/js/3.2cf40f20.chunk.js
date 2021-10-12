cfd96c5ed27fff0b6642b7808d85c623ca64d8d4
cherniaev egor <chrnyaevek@gmail.com>
2021-10-12 12:42:35 +0200

    Add general build command, drop dist folder tracking

--- application/dist/public_distribution/static/js/3.2cf40f20.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpclient_admin_distribution=this.webpackJsonpclient_admin_distribution||[]).push([[3],{12:function(i,n,t){}}]);
-//# sourceMappingURL=3.2cf40f20.chunk.js.map
--- application/dist/public_distribution/static/js/main.fa8fe6d0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpclient_admin_distribution=this.webpackJsonpclient_admin_distribution||[]).push([[0],{4:function(n,t,e){"use strict";e.r(t);var i=e(0),r=e.n(i),c=e(3),o=e.n(c),s=(e(9),e(2));Promise.all([e.e(3),e.e(4)]).then(e.bind(null,11)).then((function(n){var t=n.default;return o.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(t,{})}),document.getElementById("root"))}))},9:function(n,t,e){}},[[4,1,2]]]);
-//# sourceMappingURL=main.fa8fe6d0.chunk.js.map
