70b3da93073878eaed4439a1b4b63f292af0be35
cherniaev egor <chrnyaevek@gmail.com>
2021-10-24 16:38:51 +0200

    Update public home page

--- application/src/public_distribution/components/HomePage/HomePage.tsx
+++ application/src/public_distribution/components/HomePage/HomePage.tsx
@@ -1,37 +1,53 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
 import {
     Link,
     useHistory
 } from "react-router-dom";
+import {validateRoomExist} from "../../../core/actions/room_actions";
 
 export function HomePage() {
     const [roomId, setRoomId]: [string, any] = useState('')
     const getRoomURL = () => '/public/' + roomId
-    let history = useHistory();
+
+    const [isValid, setIsValid]: [boolean, any] = useState(false)
+
+    const history = useHistory();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                setIsValid(await validateRoomExist(roomId))
+            } catch (e) {
+                setIsValid(false)
+            }
+        })()
+    }, [roomId])
 
     return (
         <div>
-            <div className="font-big"><strong>Vítejte v aplikaci Metr!</strong></div>
-            <div className="font-middle text-secondary mb-5">Pro pokračování zadejte ID přednášky</div>
             <div className="font-middle d-flex form-group">
-                <input type="text" className="form-control mr-1" placeholder="Zadejte ID přednášky" onChange={(e) => {
-                    setRoomId(e.target.value)
-                }}/>
+                <input type="text" className="form-control mr-1" placeholder="Zadejte ID přednášky..."
+                       onChange={(e) => {
+                           setRoomId(e.target.value)
+                       }}/>
                 <button className="btn btn-success" disabled={!roomId.length} onClick={() => {
                     history.push(getRoomURL())
                 }}>
                     <FontAwesomeIcon icon={faArrowRight}/>
                 </button>
             </div>
-            <div className="font-small text-secondary">
-                {
-                    roomId.length ?
-                        <Link to={getRoomURL()}>{window.location.host}{getRoomURL()}</Link> :
-                        <i>The best is yet to come...</i>
-                }
-            </div>
+            {
+                roomId.length ?
+                    <div className="font-small text-secondary position-absolute">
+                        {
+                            isValid ?
+                                <Link to={getRoomURL()}>Hlasování</Link> :
+                                <span>Přednáška neexistuje...</span>
+                        }
+                    </div> : null
+            }
         </div>
     )
 }
