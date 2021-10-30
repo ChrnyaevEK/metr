1f408b174a376fcfe30290a26a93f0f8ab989483
cherniaev egor <chrnyaevek@gmail.com>
2021-10-30 19:10:38 +0200

    Fix slider coloring

--- application/src/components/admin_distribution/Widgets/Slider/Slider.tsx
+++ application/src/components/admin_distribution/Widgets/Slider/Slider.tsx
@@ -5,11 +5,11 @@ import {displayOptions} from "../../../../share";
 export function Slider(props: {
     question: QuestionType,
 }) {
-    const [rangeStyle, setRangeStyle] = useState(displayOptions[props.question.display_option].color_shifter(props.question.rate))
     return (
         <div className="d-flex flex-column mb-2">
             <div className="font-middle text-truncate font-weight-bold">{props.question.value}</div>
-            <input style={{'filter': rangeStyle}} type="range" className="admin-slider" min={0} max={100}
+            <input style={{'filter': displayOptions[props.question.display_option].color_shifter(props.question.rate)}}
+                   type="range" className="admin-slider" min={0} max={100}
                    value={props.question.rate} readOnly={true}/>
             <div className="d-flex justify-content-between font-tiny text-secondary">
                 {displayOptions[props.question.display_option].text_equivalents
