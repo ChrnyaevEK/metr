4e0d037624bdb9bdb735c078a17d7e8ca0bcd581
cherniaev egor <chrnyaevek@gmail.com>
2021-10-30 13:32:11 +0200

    Update to https and wss

--- application/src/share.tsx
+++ application/src/share.tsx
@@ -2,8 +2,8 @@ export const QUESTION_LIMIT = 5  // Limit of questions
 export const RECONNECT_TIMEOUT = 2000  // Timeout for reconnection to server
 export const SEARCH_VALIDATION_TIMEOUT = 1000  // Timeout when looking for room
 export const ANSWER_SAVE_TIMEOUT = 1000  // Timeout for saving students answers
-export const BASE_API_URL = process.env.NODE_ENV == 'development' ? 'http://localhost:8000/api' : `http://${window.location.host}/api`
-export const BASE_WS_URL = process.env.NODE_ENV == 'development' ? 'ws://localhost:8000' : `ws://${window.location.host}`
+export const BASE_API_URL = process.env.NODE_ENV == 'development' ? 'http://localhost:8000/api' : `https://${window.location.host}/api`
+export const BASE_WS_URL = process.env.NODE_ENV == 'development' ? 'ws://localhost:8000' : `wss://${window.location.host}`
 export const DEFAULT_DISPLAY_OPTION = 'numeric_range_optimum'
 
 export const displayOptions: IDisplayOptions = {
