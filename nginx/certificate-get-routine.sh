f95744d3051f628ff927c3cf636580dea7472076
cherniaev egor <chrnyaevek@gmail.com>
2022-01-28 23:05:18 +0100

    Move nginx to upper level

--- nginx/certificate-get-routine.sh
+++
@@ -1,7 +0,0 @@
-# Script to generate SSL certificates
-# https://mindsers.blog/post/https-using-nginx-certbot-docker/ 
-
-# Remove HTTPS server and HTTP to HTTPS redirection from nginx config
-docker-compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d sticky-link.com
-
-# Return HTTPS server and HTTP to HTTPS redirection. Done!
--- nginx/certificate-update-routine.sh
+++
@@ -1,3 +0,0 @@
-# This script renew SSL certificates 
-
-docker compose run --rm certbot renew
