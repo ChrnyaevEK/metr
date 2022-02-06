4cf82612ec57ae9f8e1d322a8f057e6fe986316c
cherniaev egor <chrnyaevek@gmail.com>
2022-02-06 19:21:20 +0100

    Update contact page

--- application/web/src/components/ContactPage/ContactPage.tsx
+++ application/web/src/components/ContactPage/ContactPage.tsx
@@ -3,13 +3,13 @@ export function ContactPage() {
         <div>
             <p className="h1 font-weight-bold">Kontakt</p>
             <p className="text-secondary font-middle">
-                Potřebuješ s něčím poradit? Máš skvělý nápad na zlepšení aplikace? Nevahej nás kontaktovat!
+                Potřebuješ s něčím poradit? Máš skvělý nápad na zlepšení aplikace? Neváhej nás kontaktovat!
             </p>
             <div className="d-flex flex-column mb-2 text-secondary">
-                <span>Our Facebook group <a href="https://www.facebook.com/groups/metrbot" target="_blank"
+                <span>Facebook skupina <a href="https://www.facebook.com/groups/metrbot" target="_blank"
                                             rel="noreferrer">Metrbot</a>
                 </span>
-                <span>Application is opensource and development is located on <a
+                <span>Aplikace je open source a celý vývoj běží zde <a
                     href="https://github.com/ChrnyaevEK/metr" target="_blank"
                     rel="noreferrer">GitHub</a>
                 </span>
